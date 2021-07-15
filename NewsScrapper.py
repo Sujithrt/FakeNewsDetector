170a613fafe45b41d406be9a498ad476641727f8
Sujithrt <47356838+Sujithrt@users.noreply.github.com>
2021-07-15 19:02:02 +0530

    Changes in NewScrapper.py

--- NewsScrapper.py
+++ NewsScrapper.py
@@ -1,49 +1,42 @@
 import requests
-import csv
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
+import pandas as pd
 
-start = 1
-end = 101
 
-headlines = []
-news = []
-target = []
-user_agent = UserAgent()
+def get_data():
+    df = pd.DataFrame(columns=['News', 'Target'])
+    start = 1
+    end = 501
+    for i in range(start, end):  # iterating through web pages
+        r = requests.get(f'https://www.politifact.com/factchecks/list/?page={i}', headers={"user-agent": user_agent.random})
+        soup = BeautifulSoup(r.content, 'html.parser')
+        headlines = soup.find_all('div', class_='m-statement__quote')
 
-for i in range(start, end):  # iterating through web pages
+        images = soup.find_all('div', class_='m-statement__meter')
 
-    r = requests.get(f'https://www.politifact.com/factchecks/list/?page={i}', headers={"user-agent": user_agent.chrome})
-    soup = BeautifulSoup(r.content, 'html.parser')
-    headlines = soup.find_all('div', class_='m-statement__quote')
+        for j in range(len(headlines)):  # iterating through different news headlines in a single webpage
+            info = headlines[j].find('a').text
+            info = info.replace("\n", "").replace('"', '').replace("'", "").replace(" ", " ")
+            pic = images[j].find('div', class_='c-image')
+            tar = pic.find('img', class_='c-image__thumb')  # extracting whether news is real or fake
+            tar = tar.get('alt')
+            if tar == "true":
+                tar = 1
+            else:
+                tar = 0
+            df = df.append({'News': info, 'Target': tar}, ignore_index=True)
 
-    images = soup.find_all('div', class_='m-statement__meter')
+        print(f'page {i} scraped')
 
-    for j in range(len(headlines)):  # iterating through different news headlines in a single webpage
-        info = headlines[j].find('a').text
-        # info = headlines[j].find('div', {"class": "m-statement__quote"})
-        news.append(info)  # extracting actual news headline text
-        news[j] = news[j].strip('\n')
-        news[j] = news[j].strip('"')
-        pic = images[j].find('div', class_='c-image')
-        tar = pic.find('img', class_='c-image__thumb')  # extracting whether news is real or fake
-        target.append((tar.get('alt')))
+        if i % 50 == 0:
+            df.to_csv("fake_news_data1.csv", index=False)
+            print(f"Checkpoint Save: {i} row/s saved")
 
-    print(f'page {i} scraped')
+    df.to_csv("fake_news_data1.csv", index=False)
+    print(f"Final Save: {i} row/s saved")
 
-for i in range(len(target)):  # converting real to 1 and fake to 0
-    if target[i] == "true":
-        target[i] = 1
-    else:
-        target[i] = 0
 
-headers = ['News', 'Target']
-with open('Fake_News_Data_Testing.csv', 'a', newline='', encoding='utf-8') as csv_file:  # saving the scraped data to a csv file
-    writer = csv.writer(csv_file)
-    writer.writerow(headers)
-    for i in range(len(target)):
-        writer.writerow([news[i], target[i]])
-
-print("Saved to the file")
-print(len(news))
-print(len(target))
+if __name__ == "__main__":
+    user_agent = UserAgent()
+    get_data()
